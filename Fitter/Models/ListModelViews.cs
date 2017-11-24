2740a9b89f02a0cf8a37eb602785d056efd5fbc1
egon <egon.malfroy@student.howest.be>
2017-11-24 21:33:01 +0100

    fese

---
+++ Fitter/Models/ListModelViews.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fitter.Models
+{
+    public class ListModelViews
+    {
+        List<ModelViewSport> ModelSports;
+
+    }
+
+}
