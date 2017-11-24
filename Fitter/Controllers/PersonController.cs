d93578c237ff21448ad8662ac92b33e788023d4f
Ddc <decraemerdante@gmail.com>
2017-11-24 16:46:44 +0100

    Model generated from db

---
+++ Fitter/Controllers/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fitter.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
