3d3087d4ff154fd4dbda0ca6a144149d040d9e80
Ddc <decraemerdante@gmail.com>
2017-11-24 18:50:02 +0100

    Lots of changes....

---
+++ Fitter/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Fitter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fitter.Controllers
+{
+    public class RegisterController : Controller
+    {
+        CoachDBEntities db = new CoachDBEntities();
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fitter/Scripts/Register.js
@@ -0,0 +1,12 @@
+﻿$(".check input").change(function () {
+    console.log("test");
+    if ($(".check input").is(":checked")) {
+        $(".teacher").css("visibility", "visible");
+    }
+    else {
+        $(".teacher").css("visibility", "hidden");
+    }
+});
+
+
+
